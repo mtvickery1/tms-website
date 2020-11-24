35e3198d07f312e7164ec5f5a783424d697179ab
mv04 <mtvickery1@gmail.com>
2020-11-24 11:50:34 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1711a776af69cc68c95c65ee8e60739e.js"
+  "/precache-manifest.acf666675dd7612e9435e9363274efc8.js"
 );
 
 self.addEventListener('message', (event) => {
